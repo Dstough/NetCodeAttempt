3dda2dc6485000562d00d6b33198243c77008805
Daniel <Daniel.Stoughton@gmail.com>
2018-11-18 16:08:35 -0600

    Added Base Applications
    Added the core application objects

---
+++ Client/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+namespace NetCodeAttempt
+{
+    public class Client
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press ESC to stop");
+            try
+            {
+                do
+                {
+                    while (!Console.KeyAvailable)    
+                    {
+                    }
+                    switch(Console.ReadKey(true).Key)
+                    {
+                        case ConsoleKey.A:
+                            
+                        default:break;
+                    }
+                }
+                while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
