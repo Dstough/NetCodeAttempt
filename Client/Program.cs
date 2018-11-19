63fb17ee60e60c32eb0e429ebee0e01776e6dcea
Daniel Stoughton <Daniel.Stoughton@gmail.com>
2018-11-19 16:00:00 -0600

    Got the program to send 1 packet
    There is still an issue with the server only recieving 1 packet then stopping.

---
+++ Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+namespace NetCodeAttempt
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var message = "Hello World";
+                var hostName = "localhost";
+                var hostPort = 20000;
+                var client = new UdpClient();
+
+                client.Connect(hostName, hostPort);
+
+                Console.WriteLine("Press ESC to stop");
+                while (Console.ReadKey(true).Key != ConsoleKey.Escape)
+                {
+                    client.Send(Encoding.ASCII.GetBytes(message), message.Length);
+                    Console.WriteLine("Sent Message.");
+                }
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -6,36 +6,47 @@ namespace NetCodeAttempt
 {
     public class Program
     {
+
+        public struct UdpState
+        {
+            public UdpClient client;
+            public IPEndPoint endPoint;
+        }
         static void Main(string[] args)
         {
             try
             {
                 var serverPort = 20000;
-                var server = new UdpClient(serverPort);
-                server.BeginReceive(DataRecieved, server);
+
+                var endPoint = new IPEndPoint(IPAddress.Any, serverPort);
+                var client = new UdpClient(endPoint);
+                var state = new UdpState();
+                state.endPoint = endPoint;
+                state.client = client;
+
+                client.BeginReceive(new AsyncCallback(ReceiveCallback), state);
+
                 Console.WriteLine("Press ESC to stop");
-                do
+                while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
                 {
-                    while (!Console.KeyAvailable)
-                    {
-                    }
                 }
-                while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+                client.Close();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
         }
- 
-        private static void DataRecieved(IAsyncResult result)
-        {
-            var client = (UdpClient)result.AsyncState;
-            var receivedIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
-            var receivedBytes = client.EndReceive(result, ref receivedIpEndPoint);
-            var receivedText = ASCIIEncoding.ASCII.GetString(receivedBytes);
 
-            Console.Write(receivedIpEndPoint + ": " + receivedText + Environment.NewLine);
+        public static void ReceiveCallback(IAsyncResult ar)
+        {
+            var u = (UdpClient)((UdpState)(ar.AsyncState)).client;
+            var e = (IPEndPoint)((UdpState)(ar.AsyncState)).endPoint;
+
+            var receiveBytes = u.EndReceive(ar, ref e);
+            var receiveString = Encoding.ASCII.GetString(receiveBytes);
+
+            Console.WriteLine("Received: {0}", receiveString);
         }
     }
 }
